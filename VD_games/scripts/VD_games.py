c6803336579388338418c6cfc372de974a247863
root <root@ASUS.localdomain>
2026-02-13 23:04:02 +0300

    Выполнена практическая работа №1

---
+++ VD_games/scripts/VD_games.py
@@ -0,0 +1,8 @@
+def main():
+    print("Модуль VD_games")
+    
+def game_function():
+    return "Игровая функция запущена"
+
+if __name__ == "__main__":
+    main()
---
+++ VD_games/scripts/VD_main.py
@@ -0,0 +1,9 @@
+from VD_games.scripts import VD_games
+
+def main():
+    print("Точка входа VD_main")
+    result = VD_games.game_function()
+    print(result)
+
+if __name__ == "__main__":
+    main()
